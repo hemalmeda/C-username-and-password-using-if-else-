decb7cbf7c6b5bbcc21a27967c37b86c0bd86aeb
hemalmeda <84007831+hemalmeda@users.noreply.github.com>
2021-06-18 08:45:47 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  string a,b;
+  cout << "username: ";
+  cin >> a;
+  if (a == "coder123")
+    {
+     cout<<"password : ";
+     cin>> b;
+     if(b=="jscpp"){
+         cout<<"correct!";
+     }
+     else{
+         cout << "wrong password try again\n Password:";
+         cin>>b;
+         
+         
+     }
+    }
+  else
+    {
+      cout << "Wrong username try again\n username:";
+      cin>>a;
+       if (a == "hemalmeda@gmail.com")
+    {
+     cout<<"password : ";
+     cin>> b;
+     if(b=="HEMAL@!@#$"){
+         cout<<"correct!";
+     }
+     else{
+         cout << "wrong password";
+     }
+    }
+      
+    }
+}
+
+
+
